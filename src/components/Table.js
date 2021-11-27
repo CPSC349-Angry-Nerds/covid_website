6a73da2f54d2ed81f51e1593ec4967f540622759
Uyen-nt-Le <uyenle8897@csu.fullerton.edu>
2021-11-26 18:43:49 -0800

    update

---
+++ src/components/Table.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Table = () => {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>SortOptionHere</th>
+                    <th>Yesterday</th>
+                    <th>Today</th>
+                    <th>Tomorrow</th>
+                </tr>
+                <tr>
+                    <th>Confirmed</th>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                </tr>
+                <tr>
+                    <th>Active</th>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                </tr>
+                <tr>
+                    <th>Recovered</th>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                </tr>
+                <tr>
+                    <th>Fatal</th>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                    <td>insert data</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default Table
