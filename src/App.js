4eb75bc87fe54d5e0f4609b20d7ddd10a533445e
tukevin2 <tukevin@csu.fullerton.edu>
2021-11-29 21:20:38 -0800

    Fixed banner issue
    Updated banner

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Covid from './components/covid-19/Covid';
 
 function App() {
   return (
+    <div><div><Covid/></div>
     <div className="container-lg App pb-5 px-5">
       <h1> </h1>
       <div className=" px-5">
-        <Covid />
         <Map />
       </div>
       <Table/>
       <Graph/>
-    </div>
+    </div></div>
   );
 }
 
